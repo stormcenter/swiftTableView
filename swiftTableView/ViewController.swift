37c3fcd3e7b26222baa0fdb7263a194e5bd6ff3c
Chi Zhang <zhangchi1213@gmail.com>
2014-06-04 23:19:54 +0800

    init commit

---
+++ swiftTableView/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  swiftTableView
+//
+//  Created by Chi Zhang on 14/6/4.
+//  Copyright (c) 2014年 Chi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    
+    @IBOutlet
+    var tableView: UITableView
+    var items: String[] = ["China", "USA", "Russia"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+}
+
---
+++ swiftTableViewTests/swiftTableViewTests.swift
@@ -0,0 +1,35 @@
+//
+//  swiftTableViewTests.swift
+//  swiftTableViewTests
+//
+//  Created by Chi Zhang on 14/6/4.
+//  Copyright (c) 2014年 Chi. All rights reserved.
+//
+
+import XCTest
+
+class swiftTableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
